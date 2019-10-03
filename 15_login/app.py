cd17217b707d1df7da9bba68cda6c16a453a5931
emz1003 <emzhang1003@gmail.com>
2019-10-03 07:53:53 -0400

    update 15_login

--- 15_login/app.py
+++ 15_login/app.py
@@ -1,31 +1,66 @@
-from flask import Flask, render_template, request, redirect, url_for
+#Amanda Zheng and Emily Zhang (Team CookieMonster)
+#SoftDev1 pd10
+#K15 -- Do I Know You?
+#2019-10-02
+import os
+from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/", methods=['GET'])
 def homepage():
     return render_template("home.html")
 
 
 @app.route("/error1")
 def error1():
-    return "something else"
+    return render_template("error1.html")
+
 
 @app.route("/error2")
 def error2():
-    return "error 2"
+    return render_template("error2.html")
+
 
 @app.route("/error3")
 def error3():
-    return "error 3"
+    return render_template("error3.html")
+
+
+@app.route("/redirecting-from-login")
+def redirecting():
+        user = "CookieMonster"
+        pa = "SeasameStreet"
+        if(request.args["username"] == user):
+            if (request.args["password"] == pa):
+                return redirect(url_for('welcome'))
+            else:
+                return redirect(url_for('error2'))
+        else:
+            if (request.args["password"] == pa):
+                return redirect(url_for('error1'))
+            else:
+                return redirect(url_for('error3'))
+
 
 @app.route("/welcome")
 def welcome():
-    return "welcome"
+    #random_bytes = os.urandom(64)
+    #app.secret_key = b64encode(random_bytes).decode('utf-8')
+    #session[app.secret_key]=
+    return render_template("welcome.html")
+
 
 @app.route("/loggedout")
 def logged_out():
-    return "you are logged out"
+    return render_template("home.html")
+
+
+@app.route("/redirecting-from-welcome")
+def redirector():
+        return redirect(url_for('logged_out'))
+
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
-
