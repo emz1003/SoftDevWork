1f7c77cf982923d969f56f76896a77b4a14f65ef
emz1003 <emzhang1003@gmail.com>
2019-11-12 17:58:42 -0500

    modify according to william's post

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -8,9 +8,8 @@ app = Flask(__name__)
 @app.route("/")
 def root():
     m = urllib3.PoolManager()
-    u = m.urlopen('GET', "https://api.nasa.gov/planetary/apod?api_key=bq9gvCRwxkSdY9KcFjOcfDD51KcdhmahpVMnI1GJ")
-    response = u.data.decode("UTF-8")
-    data = json.loads( response )
+    response = m.request('GET', "https://api.nasa.gov/planetary/apod?api_key=bq9gvCRwxkSdY9KcFjOcfDD51KcdhmahpVMnI1GJ")
+    data = json.loads( response.data )
     return render_template("index.html", pic = data['url'], ex = data['explanation'], title = data['title'])
 
 if __name__ == "__main__":
