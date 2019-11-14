cfb409f40cd4222fcbd0167866cabd6c09350500
emz1003 <emzhang1003@gmail.com>
2019-11-14 01:05:51 -0500

    finished assignment

--- 25_restrio/app.py
+++ 25_restrio/app.py
@@ -1,26 +1,35 @@
-#OpenFDA, OMDb, Petfinder
-#SyK4634D4oYZzQ0pLitUecqQ3woLNUdc2m7auU4i - openFDA
-#<Frist> <Lsat>
-#SoftDev1 pd<p>
-#K<n> -- <Title/Topic/Summary>
-#<yyyy>-<mm>-<dd>
+#Emily Zhang
+#SoftDev1 pd 1
+#K25 Getting More REST
+#2019-11-14
 
-import urllib, json
+import urllib3, json
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/openfda')
-def openFDA():
-    #https://api_basics.fda.gov/drug/event.json?api_key=yourAPIKeyHere&search=
-    #https://api.fda.gov/drug/event.json?limit=1
-    u = urllib.urlopen('https://api.fda.gov/drug/event.json?api_key=SyK4634D4oYZzQ0pLitUecqQ3woLNUdc2m7auU4i&limit=1')
-    url = u.read()
-    data = json.loads(url)
-    return render_template('index.html', )
+@app.route('/')
+def root():
 
-@app.route('/omdb')
-def OMDb():
+    m = urllib3.PoolManager()
+    quotes = m.request('GET', 'http://quotes.rest/qod.json')
+    qdata = json.loads(quotes.data)
+
+    spacex = m.request(
+        'GET', 'https://api.spacexdata.com/v3/launches?limit=1&offset=5')
+    sxdata = json.loads(spacex.data)
+
+    darksky = m.request(
+        'GET', 'https://api.darksky.net/forecast/4a53d67435477c52cc1126c018404363/37.8267,-122.4233')
+    dsdata = json.loads(darksky.data)
+
+    return render_template(
+        'index.html',
+        quote=qdata['contents']['quotes'][0]['quote'],
+        author=qdata['contents']['quotes'][0]['author'], 
+        img=sxdata[0]['links']['mission_patch'],
+        description=dsdata['alerts'][0]['description'])
+
 
 if __name__ == '__main__':
     app.debug = True
