e0c5b41a2b93445443cd6d152ade965c6228ab1c
emz1003 <emzhang1003@gmail.com>
2020-03-10 00:59:06 -0400

    add start of k10

---
+++ spring/10_mongo/mongo.py
@@ -0,0 +1,14 @@
+# Emily Zhang and Yifan Wang
+# SoftDev pd1
+# K10 -- Import/Export Bank
+# 2020-03-04
+
+from pymongo import MongoClient
+
+file = open("pokedex.json")
+client = MongoClient()
+client = MongoClient('localhost', 27017)
+db = client.pokedex
+collection = db.pokemon
+pokemon = file.read()
+db.pokemon.insert_many(pokemon)
